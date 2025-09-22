81c52e5c64ab207f7407e95841b4762ffb9ac593
Lucas <77959491+19LBates@users.noreply.github.com>
2025-09-22 15:08:54 +0100

    Delete src directory

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment'
-
-export const title = 'capabletechteam'
-export const description = 'capabletechteam site for nerds'
-export const url = dev ? 'http://localhost:5173/' : 'https://joyofcode.xyz/'
